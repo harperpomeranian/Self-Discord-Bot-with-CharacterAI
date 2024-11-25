bab43167563c036cdf543b5d07c0e393437cfe08
harperpompom <cl30whitfield2005@gmail.com>
2024-11-25 09:45:26 +0800

    - Removed useless `discord2cai` global var - Added support for servers in the whitelist - New way of commanding the bot   - Might add a `trusted` list in a few - Added more debugging statements - Moved `is_generating_reply` setters to the actual function (moved to `reply_to_discord` instead of in `on_message` - Updated `load_configuration`

--- main.py
+++ main.py
@@ -19,10 +19,9 @@ character_id: str = getenv('CHARACTER_ID')
 character_name: str = None
 cai_chat = None
 cai_me = None
-discord2cai: dict[str, dict[str, str]] = {}
 
 # Other
-is_message_processing = False
+is_generating_reply = False
 server_or_channel_whitelist = []
 
 # Be noteful when changing this
@@ -34,51 +33,64 @@ async def on_ready() -> None:
 
 @dc_client.event
 async def on_message(message: discord.Message) -> None:
-    global is_message_processing, character_id, character_name, server_or_channel_whitelist
+    global character_id, character_name, server_or_channel_whitelist
     
     # Whitelist
-    if message.channel.id not in server_or_channel_whitelist and len(server_or_channel_whitelist) > 0:
+    whitelist_equiv = message.channel.id if not message.guild else message.guild.id
+    if whitelist_equiv not in server_or_channel_whitelist and len(server_or_channel_whitelist) > 0:
+        print(whitelist_equiv, 'is not in whitelist...')
         return
     
     if message.author == dc_client.user or not (isinstance(message.channel, (discord.channel.DMChannel)) or dc_client.user in message.mentions):
         # Custom commands
-        from_me = message.author == dc_client.user
-        if message.content.startswith('!botset ') and from_me:
+        if not message.content.startswith('-!'):
+            return
+        
+        cmd_params: list[str] = message.content.split(' ')[1:]
+        if len(cmd_params) == 0:
+            return
+        
+        cmd: str = message.content[2:message.content.find(' ')].lower()
+        from_me: bool = message.author == dc_client.user
+        
+        if cmd == 'botset':
             print('Loading new character...')
-            await load_char(message.content.replace('!botset ', ''))
+            await load_char(cmd_params[0])
             reply_message = await message.reply('Done! New CharacterAI Bot: ' + character_name)
-            await sleep(1)
+        elif cmd == 'refresh' and from_me:
+            load_configuration()
+
+        # Delete the command messages
+        await sleep(1)
+        if message.channel.permissions_for(dc_client.user).manage_messages or from_me:
             await message.delete()
-            await reply_message.delete()
-        elif message.content.startswith('!refresh') and from_me:
-            if file_exists('channel_whitelist.json'):
-                server_or_channel_whitelist = json.load(open('channel_whitelist.json', 'r'))
-                reply_message = await message.reply('Channel whitelist refreshed!')
-            else:
-                reply_message = await message.reply("There isn't a whitelist file!")
-            await sleep(1)
+        if reply_message:
             await reply_message.delete()
         return
     
-    print(f'Loading message history of {message.channel}...')
+    # Put the history loading here so the AI won't get confused
+    print(f'Loading History of {message.channel.name}')
     previous_messages: list[str] = []
     async for msg in message.channel.history():
         if msg.author != dc_client.user:
             previous_messages.append(f'{msg.author.name}: {await fix_message_content(msg.content)}')
         elif msg.content.endswith(MESSAGE_SUFFIX):
             previous_messages.append(f'{character_name}: {msg.content.replace(MESSAGE_SUFFIX, "")}')
-        
+    
+    previous_messages.append('\nYou are now talking to: ' + message.author.name)
     previous_messages = reversed(previous_messages)
     
     # Prevent the AI from overloading
-    while is_message_processing:
+    if is_generating_reply:
+        print(f'Waiting for my turn...')
+        
+    while is_generating_reply:
         await sleep(.1)
     
+    print('Randomly waiting...')
     await sleep(uniform(2.0, 6.0))
-    is_message_processing = True
     async with message.channel.typing():
         await message.reply(await reply_to_discord(message, previous_messages) + MESSAGE_SUFFIX)
-    is_message_processing = False
 
 async def fix_message_content(message: str) -> str:
     # Replace discord user ids with actual usernames
@@ -108,26 +120,17 @@ async def fix_message_content(message: str) -> str:
     return message
 
 async def reply_to_discord(message: discord.Message, message_history: list[str]) -> str:
-    if str(message.channel.id) not in discord2cai:
-        print(f'Channel {message.channel} isn\'t in memory, adding...')
-        new_chat, _ = await cai_chat.new_chat(character_id, cai_me.id)
-        await sleep(.5)  # Prevent `comment` problem
-        sent_message = await cai_chat.send_message(character_id, new_chat.chat_id, '\n'.join(message_history))
-        discord2cai[str(message.channel.id)] = {
-            'chatId': new_chat.chat_id,
-            'firstMessageId': sent_message.id
-        }
-        
-        with open('dc2cai.json', 'w') as file:
-            json.dump(discord2cai, file)
-
-        return sent_message.text
+    global is_generating_reply
     
-    print('Found channel in memory')
-    chat_info: dict[str, str] = discord2cai[str(message.channel.id)]
-    edited_message = await cai_chat.edit_message(chat_info['chatId'], chat_info['firstMessageId'], '\n'.join(message_history))
-    print(edited_message.text)
-    return edited_message.text
+    is_generating_reply = True
+    print(f'Replying to `{message.content}`...')
+    
+    new_chat, _ = await cai_chat.new_chat(character_id, cai_me.id)
+    # await sleep(.5)  # Prevent `comment` problem
+    sent_message = await cai_chat.send_message(character_id, new_chat.chat_id, '\n'.join(message_history))
+    
+    is_generating_reply = False
+    return sent_message.text
 
 async def load_char(char_id: str) -> None:
     global character_id, character_name
@@ -136,27 +139,24 @@ async def load_char(char_id: str) -> None:
     character_id = char_id
 
 def load_configuration() -> None:
-    global discord2cai, server_or_channel_whitelist
+    global server_or_channel_whitelist
     
-    if file_exists('dc2cai.json'):
-        discord2cai = json.load(open('dc2cai.json', 'r'))
-        
     if file_exists('whitelist.json'):
         server_or_channel_whitelist = json.load(open('whitelist.json', 'r'))
 
 async def main() -> None:
     global cai_client, cai_chat, cai_me, character_name
     
+    load_configuration()
+    
     # Login and setup CharacterAI
     cai_client = aiocai.Client(getenv('CHARACTER_AI_TOKEN'))
     cai_me = await cai_client.get_me()
     cai_chat = await cai_client.connect()
-    await load_char(character_id)
     
+    await load_char(character_id)
     print('CharacterAI Bot:', character_name)
     
-    load_configuration()
-
     # Start Self-Discord Bot
     await create_task(dc_client.start(getenv('DISCORD_TOKEN')))
 
